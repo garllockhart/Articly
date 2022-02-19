78d28b3d154fc540ddff98dbeec2039b734d334a
Garly Nugraha <98641173+garllockhart@users.noreply.github.com>
2022-02-19 09:50:54 +0700

    Change User Seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +17,30 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $administrator = User::create([
+            'name' => 'Administrator Articly',
+            'image' => 'default.png',
+            'email' => 'administrator@articly.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('1234567890'),
+            'remember_token' => Str::random(10),
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        $administrator->assignRole('Administrator');
+
+        $write = User::create([
+            'name' => 'Garlockhart',
+            'image' => 'default.png',
+            'email' => 'garllockhart@articly.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('1234567890'),
+            'remember_token' => Str::random(10),
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        $write->assignRole('Writer');
     }
 }
